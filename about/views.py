8057ee9e994c069181e8bfb6f09739a9a64cdde8
gaurav jagpal <gauravjagpal@hotmail.com>
2024-08-24 17:53:43 +0000

    add edit and delete functions

--- about/views.py
+++ about/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
-##from .models import Post
+from .forms import CollaborateForm
 
 
 # Create your views here.
@@ -10,11 +11,21 @@ def about(request):
     about Gaurav Jagpal
     """
     
-    about = About.objects.all().order_by('-updated_on').first()
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(request, messages.SUCCESS, "Collaboration request received! I endeavour to respond within 2 working days.")
+        
 
+    about = About.objects.all().order_by('-updated_on').first()
+    collaborate_form = CollaborateForm()
+    
     return render(
         request,
         "about/about.html",
-        {"about": about},
+        {"about": about,
+        "collaborate_form": collaborate_form,
+         },
     )
 
